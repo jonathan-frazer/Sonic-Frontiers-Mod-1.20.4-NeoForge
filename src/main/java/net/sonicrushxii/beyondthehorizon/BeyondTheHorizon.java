813f75b49845e053a7dd23b6f02cfee5fa85a63a
SonicRushXII <jonat146@gmail.com>
2025-04-03 19:44:27 +0530

    Getting Started with NeoForge

--- src/main/java/net/sonicrushxii/beyondthehorizon/BeyondTheHorizon.java
+++ src/main/java/net/sonicrushxii/beyondthehorizon/BeyondTheHorizon.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.sonicrushxii.beyondthehorizon;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -32,11 +32,11 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(BeyondTheHorizon.MODID)
+public class BeyondTheHorizon
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "beyondthehorizon";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -66,7 +66,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus)
+    public BeyondTheHorizon(IEventBus modEventBus)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
